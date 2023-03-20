c784bd289e5bde1e82705fb6fac64d67430dfc3b
CutzuDev <alexfarkas05@gmail.com>
2023-03-20 22:20:35 +0200

    Updates

--- src/components/Lists.jsx
+++ src/components/Lists.jsx
@@ -1,88 +1,24 @@
 export let PaidPacks = [
   {
-    name: "Pack1",
-    link: "https://media.discordapp.net/attachments/873100563687018556/1085280467873452042/image.png",
+    name: "Portal Pack",
+    link: "https://media.discordapp.net/attachments/1087464099237068902/1087464969735503892/image.png",
   },
   {
-    name: "Pack2",
-    link: "https://media.discordapp.net/attachments/873100563687018556/1085280467873452042/image.png",
-  },
-  {
-    name: "Pack3",
-    link: "https://media.discordapp.net/attachments/873100563687018556/1085280467873452042/image.png",
-  },
-  {
-    name: "Pack4",
-    link: "https://media.discordapp.net/attachments/873100563687018556/1085280467873452042/image.png",
-  },
-  {
-    name: "Pack4",
-    link: "https://media.discordapp.net/attachments/873100563687018556/1085280467873452042/image.png",
-  },
-  {
-    name: "Pack4",
-    link: "https://media.discordapp.net/attachments/873100563687018556/1085280467873452042/image.png",
-  },
-  {
-    name: "Pack4",
-    link: "https://media.discordapp.net/attachments/873100563687018556/1085280467873452042/image.png",
-  },
-  {
-    name: "Pack4",
-    link: "https://media.discordapp.net/attachments/873100563687018556/1085280467873452042/image.png",
-  },
-  {
-    name: "Pack4",
-    link: "https://media.discordapp.net/attachments/873100563687018556/1085280467873452042/image.png",
+    name: "Blue Pack",
+    link: "https://media.discordapp.net/attachments/1087464099237068902/1087467728669003897/image.png",
   },
 ];
 export let FreePacks = [
   {
-    name: "Pack FREE",
-    link: "https://media.discordapp.net/attachments/873100563687018556/1085280467873452042/image.png",
+    name: "Orange Pack",
+    link: "https://media.discordapp.net/attachments/1087465346702786580/1087465612604878959/image.png",
     download:
-      "https://cdn.discordapp.com/attachments/873100563687018556/1085280468607447050/3kSpecial.rar",
+      "https://cdn.discordapp.com/attachments/1087464150927687771/1087470221255782432/OrangeTexturePack.rpf",
   },
   {
-    name: "Pack FREE",
-    link: "https://media.discordapp.net/attachments/873100563687018556/1085280467873452042/image.png",
+    name: "RGB V1 Pack",
+    link: "https://media.discordapp.net/attachments/1087465346702786580/1087465717663801456/BlackWithRGBPack.png",
     download:
-      "https://cdn.discordapp.com/attachments/873100563687018556/1085280468607447050/3kSpecial.rar",
-  },
-  {
-    name: "Pack FREE",
-    link: "https://media.discordapp.net/attachments/873100563687018556/1085280467873452042/image.png",
-    download:
-      "https://cdn.discordapp.com/attachments/873100563687018556/1085280468607447050/3kSpecial.rar",
-  },
-  {
-    name: "Pack FREE",
-    link: "https://media.discordapp.net/attachments/873100563687018556/1085280467873452042/image.png",
-    download:
-      "https://cdn.discordapp.com/attachments/873100563687018556/1085280468607447050/3kSpecial.rar",
-  },
-  {
-    name: "Pack FREE",
-    link: "https://media.discordapp.net/attachments/873100563687018556/1085280467873452042/image.png",
-    download:
-      "https://cdn.discordapp.com/attachments/873100563687018556/1085280468607447050/3kSpecial.rar",
-  },
-  {
-    name: "Pack FREE",
-    link: "https://media.discordapp.net/attachments/873100563687018556/1085280467873452042/image.png",
-    download:
-      "https://cdn.discordapp.com/attachments/873100563687018556/1085280468607447050/3kSpecial.rar",
-  },
-  {
-    name: "Pack FREE",
-    link: "https://media.discordapp.net/attachments/873100563687018556/1085280467873452042/image.png",
-    download:
-      "https://cdn.discordapp.com/attachments/873100563687018556/1085280468607447050/3kSpecial.rar",
-  },
-  {
-    name: "Pack FREE",
-    link: "https://media.discordapp.net/attachments/873100563687018556/1085280467873452042/image.png",
-    download:
-      "https://cdn.discordapp.com/attachments/873100563687018556/1085280468607447050/3kSpecial.rar",
+      "https://cdn.discordapp.com/attachments/1087464150927687771/1087470208228266156/BlackWithRGBPack.rpf",
   },
 ];
