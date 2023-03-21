cc06e8230dd11fb4a30d6921afd0b5ad1d19e482
CutzuDev <alexfarkas05@gmail.com>
2023-03-21 19:40:01 +0200

    404 update

--- src/pages/NFHandle.jsx
+++ src/pages/NFHandle.jsx
@@ -4,10 +4,14 @@ import { Link } from "react-router-dom";
 function NFHandle() {
   return (
     <main className="flex h-full w-full flex-1 flex-col items-center justify-center gap-8 p-8">
-      <h1 className="font-righteous text-8xl text-white">404</h1>
-      <h1 className="font-righteous text-4xl text-white">Page Not Found</h1>
+      <h1 className="bg-gradient-to-r from-yellow-200 via-yellow-500 to-yellow-800 bg-clip-text font-righteous text-8xl text-transparent">
+        404
+      </h1>
+      <h1 className="bg-gradient-to-r from-yellow-200 via-yellow-500 to-yellow-800 bg-clip-text font-righteous text-4xl text-transparent">
+        Page Not Found
+      </h1>
       <Link to={"/"}>
-        <h1 className="rounded-md border-2 border-white bg-white bg-opacity-10 px-4 py-2 font-righteous text-2xl text-white transition duration-300 ease-in-out hover:scale-95">
+        <h1 className="rounded-md border-2 border-yellow-500 bg-yellow-500 bg-opacity-10 px-4 py-2 font-righteous text-2xl text-yellow-500 transition duration-300 ease-in-out hover:scale-95">
           Go Back
         </h1>
       </Link>
